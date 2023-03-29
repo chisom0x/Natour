f050098a44c1e1e6c1bfcff99c7db1242ae53012
chisom0x <onyenankiekelvin@gmail.com>
2023-03-29 02:13:10 -0700

    Added a comment

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
+// Start express app
 const app = express();
 
 app.set('view engine', 'pug');
